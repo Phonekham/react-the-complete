5f47598dfa9787b296b21b4a920df70c8afaa686
Phonekham <phone.madridista@gmail.com>
2019-05-19 17:12:53 +0700

    validate props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 import classes from "./Person.css";
 
@@ -19,5 +20,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.PropTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default Person;
